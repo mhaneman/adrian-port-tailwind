81cb5d6f26d5ddb64562c8aae65618fd65e8604a
michaelhaneman <mhaneman@calpoly.edu>
2022-09-21 16:55:36 -0700

    added json

--- src/Components/ResumeSection.js
+++ src/Components/ResumeSection.js
@@ -5,7 +5,7 @@ import ResumeItem from './ResumeItem'
 function ResumeSection() {
   return (
     <>
-      <div className=" p-10 bg-blue-200 text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-9xl"> Journalism </div>
+      <div className=" p-10 bg-blue-100 text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-9xl"> Journalism </div>
 
       <ResumeItem
         title = "Meaning of Article"
@@ -36,7 +36,25 @@ function ResumeSection() {
         date = "Jan 1 2019"
       />
 
-      <div className=" p-10 bg-red-200 text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-9xl"> Apple Farm </div>
+      <div className=" p-10 bg-red-100 text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-9xl"> ESPN </div>
+
+      <ResumeItem
+        title = "Meaning of Article"
+        desc = "some description about your article"
+        link = ""
+        link_name = "The title of your article"
+        date = "Jan 1 2019"
+      />
+
+      <ResumeItem
+        title = "Meaning of Article"
+        desc = "some description about your article"
+        link = ""
+        link_name = "The title of your article"
+        date = "Jan 1 2019"
+      />
+
+      <div className=" p-10 bg-green-100 text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-9xl"> Marketing </div>
 
       <ResumeItem
         title = "Meaning of Article"
