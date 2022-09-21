32b4f142197fabd45be92d8d426035c215c77277
michaelhaneman <mhaneman@calpoly.edu>
2022-09-21 16:35:01 -0700

    fixed bio page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -13,10 +13,10 @@ export default function AboutMe() {
     <>
       <div className=" p-10 bg-green-100 text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-9xl"> Get To Know Me </div>
 
-      <div class="container mx-auto m">
-      <div class="grid grid-cols-1 md:grid-cols-2 h-2/4">
-        <div class="h-4/4">
-          <img class="w-screen h-screen object-cover object-top" src={ProfilePhoto} alt="" />
+      <div class="container mx-auto h-screen flex flex-col justify-center">
+      <div class="grid grid-cols-1 md:grid-cols-2 h-3/4">
+        <div class="">
+          <img class="w-full h-full object-cover object-top" src={ProfilePhoto} alt="" />
         </div>
         <div class="flex bg-gray-100 p-10">
           <div class="mb-auto mt-auto max-w-lg">
@@ -24,14 +24,14 @@ export default function AboutMe() {
             <p class="font-semibold mb-5">Journalist</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, aspernatur laborum nam eum culpa eos, nemo corporis possimus quam omnis, repellendus dolor! Laboriosam rerum debitis enim consequuntur sequi distinctio et.</p>
 
-            <div className="flex gap-10 items-center justify-center">
-              <button class="bg-black rounded-md py-3 px-7 mt-6 text-white">Email Me</button>
+            <div className="flex gap-10 items-center justify-center mt-10">
+              <button class="bg-black rounded-md py-3 px-7 text-white">Email Me</button>
               <a href={linkedin_link}>
-                <FontAwesomeIcon icon={faLinkedin} size="xl"/>
+                <FontAwesomeIcon icon={faLinkedin} size="3x"/>
               </a>
 
               <a href={insta_link}>
-                  <FontAwesomeIcon icon={faInstagram} size="xl"/>
+                  <FontAwesomeIcon icon={faInstagram} size="3x"/>
               </a>
             </div>
 
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,9 +5,9 @@ export default function Hero() {
     <div className="relative overflow-hidden h-screen bg-no-repeat bg-cover bg-center bg-fixed grayscale-[50%]" style={{backgroundImage: `url(${HeroImage})`}}>
       
         <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:items-center lg:justify-between lg:py-16 lg:px-8 mt-64">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-9xl">
+          <h2 className="text-3xl font-bold tracking-tight text-green-500 sm:text-9xl">
             <span className="block">Enriching Stories</span>
-            <span className="block text-green-500">Adrian Rosas</span>
+            <span className="block text-white">Journalism</span>
           </h2>
           <div className="flex justify-center lg:mt-0 lg:flex-shrink-0">
           
